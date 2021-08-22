bb5571d97783a5be19df2d106d0fe7620b229a6e
Sapozhnikova-Ksenia <mobillka92@gmail.com>
2021-08-23 00:22:37 +0300

    social network added

---
+++ src/main/java/ru/netology/domain/SocialNetwork.java
@@ -0,0 +1,20 @@
+package ru.netology.domain;
+
+public class SocialNetwork {
+    private String id;
+    private String namePostingCompany;
+    private String urlOfPostingCompany;
+    private String namePost;
+    private int datePost;
+    private int timePost;
+    private String textPost;
+    private String urlPicture;
+    private String urlVideo;
+    private String urlToPost;
+    private int viewCounter;
+    private String hashtag;
+    public String textUserInComment;
+    private String textAnswerPostingCompanyInComments;
+
+    //+getters/setters
+}
